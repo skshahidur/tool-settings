7d51557e40bb9a94369ddb888bdb0f7f00ef83e2
Shahidur Rahman <skshahidur@gmail.com>
2025-08-21 07:37:17 +0200

    Password commands

--- bash_profile.sh
+++ bash_profile.sh
@@ -2,3 +2,8 @@
 alias ca310="conda activate py310"
 alias callms="conda activate llms"
 alias cadbt="conda activate dbt"
+
+# Password
+alias pw10="openssl rand -base64 10"
+alias pw15="openssl rand -base64 15"
+alias pw20="openssl rand -base64 20"
